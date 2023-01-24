ea9af570879b81dac720a76f4094d41b8761c575
Mkhitaryan Victoria <viktoriya.mkhitaryan@glowbyteconsulting.com>
2023-01-25 02:51:28 +0300

    added sort strings task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,16 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+  const resultArray = [...arr];
 
+  if (param === "asc") {
+    return resultArray.sort((a, b) =>
+      a.localeCompare(b, "ru", { caseFirst: "upper" })
+    );
+  } else if (param === "desc") {
+    return resultArray.sort((a, b) =>
+      b.localeCompare(a, "ru", { caseFirst: "upper" })
+    );
+  }
 }
