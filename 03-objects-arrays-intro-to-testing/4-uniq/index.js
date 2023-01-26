24a01f5cd9f3fe42dfac2761a2f5fb56944082a9
Mkhitaryan Victoria <viktoriya.mkhitaryan@glowbyteconsulting.com>
2023-01-26 23:16:37 +0300

    added uniq task

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,10 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (!arr) return [];
 
+  return arr.reduce((acc, currentValue) => {
+    if (acc.includes(currentValue)) return acc;
+    return [...acc, currentValue];
+  }, []);
 }
