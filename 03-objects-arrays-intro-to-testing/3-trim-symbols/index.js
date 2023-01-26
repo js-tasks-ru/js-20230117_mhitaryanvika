ef48f9371f9b3d2aa7169275f1097eb7ea015306
Mkhitaryan Victoria <viktoriya.mkhitaryan@glowbyteconsulting.com>
2023-01-26 20:46:56 +0300

    added trim symbols task

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,19 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) return "";
+  else if (!size) return string;
 
+  let counter = 0;
+  let resultString = "";
+  string.split("").forEach((symbol) => {
+    if (resultString[resultString.length - 1] === symbol) {
+      counter++;
+      if (counter <= size) resultString += symbol;
+    } else {
+      counter = 1;
+      resultString += symbol;
+    }
+  });
+  return resultString;
 }
